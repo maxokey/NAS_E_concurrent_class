c5234ec49d48dd8d99f814dd819b3c2b5fc1e1c9
Maxim Klyuchko <maxok@uw.edu>
2024-07-21 06:01:14 -0700

    all works!

--- pN/main.java
+++ pN/main.java
@@ -1,50 +1,53 @@
 class DiningPhilosophers {
-
     Object[] forkLocks = new Object[5];
     boolean[] isTaken = new boolean[5];
 
-    public DiningPhilosophers() {
-        for(int i = 0; i < 5; i++) forkLocks[i] = new Object();
-    }
-
-    // call the run() method of any runnable to execute its code
-    public void wantsToEat(int philosopher,
-                           Runnable pickLeftFork,
-                           Runnable pickRightFork,
-                           Runnable eat,
-                           Runnable putLeftFork,
-                           Runnable putRightFork) throws InterruptedException {
-        if(philosopher % 2 == 0) {
-            // System.out.printf("right for ph #%d\n", philosopher);
-            pickFork(philosopher, pickRightFork);
-            // System.out.printf("left for ph #%d\n", philosopher);
-            pickFork((philosopher + 1) % 5, pickLeftFork);
-        } else {
-            // System.out.printf("left for ph #%d\n", philosopher);
-            pickFork((philosopher + 1) % 5, pickLeftFork);
-            // System.out.printf("right for ph #%d\n", philosopher);
-            pickFork(philosopher, pickRightFork);
-        }
-        eat.run();
-        putFork(philosopher, putRightFork);
-        putFork((philosopher + 1) % 5, putLeftFork);
-    }
-
-    private void pickFork(int n, Runnable pickFunc) throws InterruptedException {
+    void pickFork(int n, Runnable pickFunc, StringBuilder logBuilder) throws InterruptedException {
         synchronized(forkLocks[n]){
             while(isTaken[n]){
                 forkLocks[n].wait();
             }
-            isTaken[n] = true;
+            isTaken[n]= true;
         }
-        pickFunc.run();
+        synchronized(this){ // necessary for leetcode tester to not lose calls
+            pickFunc.run();
+        }
     }
 
-    private void putFork(int n, Runnable putFunc) {
-        putFunc.run();
+    void putFork(int n, Runnable putFunc) throws InterruptedException {
+        synchronized(this){ // necessary for leetcode tester to not lose calls
+            putFunc.run();
+        }
         synchronized(forkLocks[n]){
             isTaken[n] = false;
             forkLocks[n].notify();
         }
     }
-}
+
+    public DiningPhilosophers() {
+        for(int i = 0; i < 5; i++) {
+            forkLocks[i] = new Object();
+        }
+    }
+
+    void wantsToEat(int philosopher,
+                    Runnable pickLeftFork,
+                    Runnable pickRightFork,
+                    Runnable eat,
+                    Runnable putLeftFork,
+                    Runnable putRightFork) throws InterruptedException {
+StringBuilder logBuilder = new StringBuilder();
+        if(philosopher % 2 == 0) {
+            pickFork((philosopher + 4) % 5, pickRightFork, logBuilder);
+            pickFork(philosopher, pickLeftFork, logBuilder);
+        } else {
+            pickFork(philosopher, pickLeftFork, logBuilder);
+            pickFork((philosopher + 4) % 5, pickRightFork, logBuilder);
+        }
+        synchronized(this){ // necessary for leetcode tester to not lose calls
+            eat.run();
+        }
+        putFork((philosopher + 4) % 5, putRightFork);
+        putFork(philosopher, putLeftFork);
+    }
+};
